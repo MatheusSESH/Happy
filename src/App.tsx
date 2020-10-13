1698b7898888d2bc773bd9fedb49853070b0af84
Matheus <matheusvinicius123466@gmail.com>
2020-10-13 12:47:14 -0300

    style: font change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello peam &#60;3</h1>
-    </div>
+    <Routes />
   );
 }
 
